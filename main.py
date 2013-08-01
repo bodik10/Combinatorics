5c0624ad84fc1b347b37a2c36531297da9936a87
Bohdan <bohdan.fedys@gmail.com>
2013-08-01 22:52:50 +0300

    Write some drafts in main.py

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ Created on 1 серп. 2013
 
 @author: Бодя
 '''
+import sys
+
+from core import *
+from view import *
+
+from PySide import QtCore, QtGui
+
+class Controller:
+    def __init__(self):
+        self.v = View()
+        
+    def run(self):
+        sys.exit(app.exec_())
+
+
 
 if __name__ == '__main__':
-    pass
+    app = QtGui.QApplication(sys.argv)
+    c = Controller()
+    label = QtGui.QLabel("Hello World") # test
+    label.show()                        # test
+    c.run()
