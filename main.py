446b47f77d5aca42e0e198dc3b0f5f1fd751d9eb
Bohdan <bohdan.fedys@gmail.com>
2013-08-13 21:53:04 +0300

    Add event handlers (to view and controller)
    Program UI almost complete (but without main sequence generator in
    thread)

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Created on 1 серп. 2013
 
 @author: Бодя
 '''
-import sys
 
+import sys
 
 from view import *
 
@@ -12,12 +12,78 @@ from PySide import QtCore, QtGui
 
 class Controller:
     def __init__(self):
+        self.m = ModelMetadata()
         self.v = View()
         
+        self.status = "stoped"
+        
+        self.v.spinColumns.valueChanged["int"].connect(lambda val: setattr(self.m, "columns", val))
+        
+        self.v.tabComb.currentChanged.connect(self.handleChange)
+        self.v.plainNCustom.textChanged.connect(self.handleChange)
+        self.v.spinN.valueChanged.connect(self.handleChange)
+        self.v.spinK.valueChanged.connect(self.handleChange)
+        self.v.radioN.clicked.connect(self.handleChange)
+        self.v.radioNCustom.clicked.connect(self.handleChange)
+        
+        self.v.btnStart.clicked.connect(self.handleStart)
+        self.v.btnStop.clicked.connect(self.handleStop)
+    
     def run(self):
         sys.exit(app.exec_())
+        
+    def handleStart(self):
+        if self.status in ["stoped", "paused"]:
+            self.status = "started"
+            
+            self.v.disabledWhenStart(True)
+            self.v.btnStart.setText("Призупинити")
+            self.v.btnStart.setIcon(QtGui.QIcon(QtGui.QPixmap(":/icons/pause.png")))
+        elif self.status == "started":
+            self.status = "paused"
+            
+            self.v.btnStart.setText("Продовжити")
+            self.v.btnStart.setIcon(QtGui.QIcon(QtGui.QPixmap(":/icons/play.png")))
+            
+            self.handlePause()
+    
+    def handlePause(self):
+        pass
+    
+    def handleStop(self):
+        self.status = "stoped"
+        
+        self.v.disabledWhenStart(False)
+        
+        self.v.btnStart.setText("Старт обчислень")
+        self.v.btnStart.setIcon(QtGui.QIcon(QtGui.QPixmap(":/icons/play.png")))
+        
+    def handleChange(self):
+        if self.v.plainNCustom.isEnabled():
+            self.m.seq = self.v.plainNCustom.toPlainText().split()
+            if not self.m.seq: return
+            self.v.spinK.setMaximum(len(self.m.seq))
+        else:
+            self.m.seq = range(1, self.v.spinN.value() + 1)
+            
+        self.m.K = self.v.spinK.value()
+        
+        self.updateMetaAll()
 
-
+    def updateMetaAll(self):
+        self.m.All = self.v.currentTab.coreNumber(self.m.seq, self.m.K)
+        self.v.MetaTableUpdate(self.m)
+        #print(self.m.All)
+        
+        
+class ModelMetadata:
+    def __init__(self):
+        self.seq = self.K = self.All = None
+        
+        self.columns = 1
+        self.result = []
+    
+    
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
