caf15cb3b2e2b61343a177b2e3cd6b46436dfce8
Bohdan <bohdan.fedys@gmail.com>
2013-09-04 23:41:02 +0300

    Make complete functionality thread

--- mythread.py
+++ mythread.py
@@ -1,17 +1,48 @@
 from PySide import QtCore
 
+from time import time
+
+
 class MyThread(QtCore.QThread):
-    def __init__(self, parent):
+    def __init__(self, parent, func = None):
         QtCore.QThread.__init__(self, parent)
         self.c = parent
+        self.func = func
         
     def run(self):
-        for RES in self.c.currentTab.coreGenerator(self.c.m.seq, self.c.m.K):
-            self.c.m.result.append(RES)
+        seq = self.c.m.seq
+        K = self.c.m.K
+        
+        start = time()
+        
+        for RES in self.func(seq, K):
+            
+            #self.c.m.result.append( str(RES) )                             # [('a', 'b', 'c'), ... ]
+            self.c.m.result.append( "(" + ", ".join(map(str, RES)) + ")" )  # ('a', 'b', 'c') -> (a, b, c)
             self.c.m.complete += 1
             
-            #self.emit(QtCore.SIGNAL("upd_prog_bar(int)"), round(self.c.m.complete / self.c.m.All * 100))
-            #self.emit(QtCore.SIGNAL("upd_prog(int)"), self.c.m.complete)
+            if len(self.c.m.result)>=50000 and len(self.c.m.result)%self.c.m.columns == 0:
+                self.c.flushResult()
+                
+            percent = self.c.m.complete / self.c.m.All * 100
             
+            if percent in range(0, 101, 5):     # update progress every 5%
+                print(percent)
+                """
+                self.emit(QtCore.SIGNAL("upd_prog_bar(int)"), int(percent))
+                self.emit(QtCore.SIGNAL("upd_prog(int)"), self.c.m.complete)"""
+                
+            if percent in range(5, 101, 25):    # update left time every 25%, starting at 5%
+                print(percent)
+                """
+                delta = int(time() - start)
+                approx_left = delta / percent * 100 
+                
+                print(approx_left)
+                
+                self.emit(QtCore.SIGNAL("upd_left_time(int)"), int(approx_left))
+                
+                start = time()
+              """
             while self.c.status=="paused":
                 self.msleep(500)
